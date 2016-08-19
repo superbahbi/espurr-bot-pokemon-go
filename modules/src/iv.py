411eff9ab33f2c140830c4c540fe850dea5fe6d9
Bahbi <superbahbi@gmail.com>
2016-08-19 15:29:35 -0700

    debug

--- modules/src/iv.py
+++ modules/src/iv.py
@@ -6,11 +6,11 @@ def process(input, data ,sender):
     if data is not None:
         pogodata = data.split(' ')
         res = calc.get_ivs(pogodata[1], pogodata[2], pogodata[3], pogodata[4], pogodata[5])
-        dump(pogodata) 
+        print pogodata
+        print data
     else:
         res = "Invalid data input!\n"
         res += "e.g check \"Chansey\" 285 271 1900 False"
-
     output = {
         'input': input,
         'output': TextTemplate(str(res)).get_message(),
