e9a867ecca8145ad82d376b0770aaa8da27dc5d9
Bahbi <superbahbi@gmail.com>
2016-08-19 13:32:14 -0700

    sample data

--- modules/src/iv.py
+++ modules/src/iv.py
@@ -1,10 +1,12 @@
 from templates.text import TextTemplate
-
+from pogoiv.iv_calculator import IvCalculator
 
 def process(input, data ,sender):
+    calc = IvCalculator()
+    res = calc.get_ivs(self, "Chansey", "285", "271", "1900", powered=False):
     output = {
         'input': input,
-        'output': TextTemplate(data).get_message(),
+        'output': TextTemplate(res).get_message(),
         'success': True
     }
     return output
