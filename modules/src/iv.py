0e17a439f69ac98a55920a82ece9a4820a536cce
Bahbi <superbahbi@gmail.com>
2016-08-19 15:12:51 -0700

    if not lul

--- modules/src/iv.py
+++ modules/src/iv.py
@@ -3,7 +3,7 @@ from pogoiv.iv_calculator import IvCalculator
 
 def process(input, data ,sender):
     calc = IvCalculator()
-    if data is None:
+    if data is not None:
         pogodata = data.split(' ')
         res = calc.get_ivs(pogodata[1], pogodata[2], pogodata[3], pogodata[4], pogodata[5])
     else:
