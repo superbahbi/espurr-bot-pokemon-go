4be644c46c06b3929a444824b238a7ae0f5fb105
Bahbi <superbahbi@gmail.com>
2016-08-22 16:06:29 -0700

    iv data

--- modules/src/iv.py
+++ modules/src/iv.py
@@ -5,9 +5,7 @@ def process(input, data ,sender):
     calc = IvCalculator()
     if data is not None:
         pogodata = data.split(' ')
-        res = calc.get_ivs("Chansey", 285, 271, 1900, False)
-        print pogodata
-        print data
+        res = calc.get_ivs(pogodata[1].encode("utf-8"), pogodata[2].encode("utf-8"), pogodata[3].encode("utf-8"), pogodata[4].encode("utf-8"), pogodata[5].encode("utf-8"))
     else:
         res = "Invalid data input!\n"
         res += "e.g check \"Chansey\" 285 271 1900 False"
